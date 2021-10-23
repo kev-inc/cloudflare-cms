be4bdd943297eb9f341e6a336181980b329c8aa1
kev-inc <kevinchanxy@gmail.com>
2021-10-23 16:22:09 +0800

    added PUT

--- backend/index.js
+++ backend/index.js
@@ -57,35 +57,29 @@ router.post('/namespaces/:accountId', async request => {
 })
 
 /*
-This shows a different HTTP method, a POST.
-
-Try send a POST request using curl or another tool.
-
-Try the below curl command to send JSON:
-
-$ curl -X POST <worker> -H "Content-Type: application/json" -d '{"abc": "def"}'
+{
+    "data": {
+        "title": "My Namespace"
+    },
+    "apiKey": "Bearer xxx"
+}
 */
-router.post('/post', async request => {
-    // Create a base object with some fields.
-    let fields = {
-        asn: request.cf.asn,
-        colo: request.cf.colo,
-    }
+router.put('/namespaces/:accountId/:namespaceId', async request => {
+    const accountId = request.params.accountId
+    const namespaceId = request.params.namespaceId
+    const { data, apiKey } = await request.json()
 
-    // If the POST data is JSON then attach it to our response.
-    if (request.headers.get('Content-Type') === 'application/json') {
-        fields['json'] = await request.json()
-    }
-
-    // Serialise the JSON to a string.
-    const returnData = JSON.stringify(fields, null, 2)
-
-    return new Response(returnData, {
+    const resp = await fetch(BASE_URL + accountId + '/storage/kv/namespaces/' + namespaceId, {
+        method: 'PUT',
         headers: {
+            Authorization: apiKey,
             'Content-Type': 'application/json',
         },
-    })
+        body: JSON.stringify(data),
+    }).then(resp => resp.json())
+    return new Response(JSON.stringify(resp, null, 2))
 })
+
 
 router.all('*', () => new Response('404, not found!', { status: 404 }))
 
