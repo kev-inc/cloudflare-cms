f049f9a768a158ca9255d4ee53f146e4b9ad57ed
kev-inc <kevinchanxy@gmail.com>
2021-10-23 17:30:33 +0800

    fixed bugs

--- frontend/src/api/cloudflare-cms-api.js
+++ frontend/src/api/cloudflare-cms-api.js
@@ -1,11 +1,56 @@
 const axios = require("axios");
 
-const BASE_URL = "https://cloudflare-cms-api.kevc.workers.dev";
+// const BASE_URL = "https://cloudflare-cms-api.kevc.workers.dev";
+const BASE_URL = "http://localhost:8787";
 
 export const getNamespaces = async (apiKey, id) => {
   const resp = await axios.get(BASE_URL + `/namespaces/${id}`, {
-    params: {
-      apiKey: `Bearer ${apiKey}`,
+    // params: {
+    //   apiKey: `Bearer ${apiKey}`,
+    // },
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
+    },
+  });
+  return resp.data;
+};
+
+export const postNamespaces = async (apiKey, id, name) => {
+  const resp = await axios.post(
+    BASE_URL + `/namespaces/${id}`,
+    {
+      title: name,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return resp.data;
+};
+
+export const putNamespaces = async (apiKey, accId, nsId, name) => {
+  const resp = await axios.put(
+    BASE_URL + `/namespaces/${accId}/${nsId}`,
+    {
+      title: name,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return resp.data;
+};
+
+export const deleteNamespaces = async (apiKey, accId, nsId) => {
+  const resp = await axios.delete(`${BASE_URL}/namespaces/${accId}/${nsId}`, {
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
     },
   });
   return resp.data;
