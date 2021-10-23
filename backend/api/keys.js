78f41456d102b098cbe1cb04e604da3d9aed12ac
kev-inc <kevinchanxy@gmail.com>
2021-10-23 20:39:47 +0800

    added CRUD for values

--- backend/api/keys.js
+++ backend/api/keys.js
@@ -8,10 +8,39 @@ export const getKeys = async request => {
     const namespaceId = request.params.namespaceId
     const apiKey = request.headers.get('Authorization')
 
-    const resp = await fetch(`${BASE_URL + accountId}/storage/kv/namespaces/${namespaceId}/keys`, {
-        headers: {
-            Authorization: apiKey,
-        },
-    }).then(resp => resp.json())
+    const resp = await fetch(
+        `${BASE_URL + accountId}/storage/kv/namespaces/${namespaceId}/keys`,
+        {
+            headers: {
+                Authorization: apiKey,
+            },
+        }
+    ).then(resp => resp.json())
+    return new Response(JSON.stringify(resp, null, 2), { headers })
+}
+
+/*
+{
+    "title": "My New Key"
+}
+*/
+export const postNewKey = async request => {
+    const accountId = request.params.accountId
+    const namespaceId = request.params.namespaceId
+    const apiKey = request.headers.get('Authorization')
+    const data = await request.json()
+    const keyName = data['title']
+    const resp = await fetch(
+        `${BASE_URL +
+            accountId}/storage/kv/namespaces/${namespaceId}/values/${keyName}`,
+        {
+            method: 'PUT',
+            headers: {
+                Authorization: apiKey,
+                'Content-Type': 'text/plain',
+            },
+            body: '[]',
+        }
+    ).then(resp => resp.json())
     return new Response(JSON.stringify(resp, null, 2), { headers })
 }
